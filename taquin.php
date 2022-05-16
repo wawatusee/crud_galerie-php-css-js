5b63752afb1b7d05de89fe2a1937342aa20a072f
wawatusee <web2020.kieran.labarrere@gmail.com>
2022-05-16 15:25:53 +0200

    taquin.php, nom repertoire image adapté à crud

---
+++ taquin.php
@@ -0,0 +1,50 @@
+<?php 
+//Import du fichier json source
+$jsonImageTaquin=json_decode(file_get_contents("js/image-taquin.json"));
+//Récupération du nom de l'image source
+$nomImage=$jsonImageTaquin->image_taquin;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/taquin.css">
+    <title>Taquin</title>
+</head>
+
+<body onload="taquin()" ;>
+    <div class="c1">
+        <header>
+            <h1>Raymond Taquin</h1>
+        </header>
+        <section id="scene">
+            <div class="taquin">
+                <div class="piece"></div>
+                <div class="piece"></div>
+                <div class="piece"></div>             
+                <div class="piece"></div>             
+                <div class="piece"></div>             
+                <div class="piece"></div>             
+                <div class="piece"></div>             
+                <div class="piece"></div>             
+                <div class="piece"></div>             
+                <div class="piece"></div>             
+                <div class="piece"></div>             
+                <div class="piece"></div>             
+                <div class="piece"></div>             
+                <div class="piece"></div>             
+                <div class="piece"></div>             
+                <div id="pieceInvisible" class="piece"></div>
+            </div>
+        </section>
+        <footer>
+            <div>Image du jour</div>
+                <section id="planDeSite">
+                <?php echo $nomImage ?>
+            </section>
+        </footer>
+    </div>
+    <script type="text/javascript" src="js/taquin.js"></script>
+</body>
+</html>
